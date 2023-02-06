974182340f2f0f9abee921e9c4dd49507c0a886d
IrinaSelitskaya <119079534+IrinaSelitskaya@users.noreply.github.com>
2023-02-06 19:08:37 +0300

    Update PowerOfTwo.java

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-
+     power=12;
+     int i=0;
+     int result=1;
+     while(i<power){
+      result*=2;
+      if(result>=0){
+       System.out.println(result);
+      }else{
+       System.out.println("too much power");
+      }
+      i++;} 
     }
 }
