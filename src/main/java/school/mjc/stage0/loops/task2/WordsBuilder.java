026f7219af90a27bf7c2bc885a44bbeeac0b4777
IrinaSelitskaya <119079534+IrinaSelitskaya@users.noreply.github.com>
2023-02-07 19:59:12 +0300

    Update WordsBuilder.java

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,7 +2,9 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-     chars='J';
-     System.out.println(chars);
+     int i=0;
+     while(i<chars.length){
+      System.out.print(chars[i]);
+      i++;} 
     }
 }
